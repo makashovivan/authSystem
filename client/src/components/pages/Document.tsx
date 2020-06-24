1b2e582303b54a141f0ea7b44e4f35a5e5ebc0b1
makashovivan <osoniv@gmail.com>
2020-06-25 00:25:00 +0300

    Document component created

--- client/src/components/pages/Document.tsx
+++ client/src/components/pages/Document.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import Card from '@material-ui/core/Card';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
-const Document: React.FC = () => {
+interface IDocumentProps  {
+  text: string,
+}
+
+const useStyles = makeStyles({
+  root: {
+    margin: '0 auto',
+    marginTop: 10,
+    padding: 10,
+    width : 350,
+    height: 40,
+    textAlign: 'left',
+    backgroundColor: '#e8edea',
+  },
+  btn: {
+    margin: 10
+  }
+})
+
+const Document: React.FC<IDocumentProps> = ({text}) => {
+
+  const classes = useStyles()
+
   return (
-    <div>Document</div>
+    <div>
+      <Card className= {classes.root}>
+        {text}
+      </Card>
+    </div>
   )
 }
 
