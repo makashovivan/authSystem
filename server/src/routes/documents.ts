7727f75546918bd1a573a9019b15f1bd4d8b720e
makashovivan <osoniv@gmail.com>
2020-06-25 00:32:06 +0300

    documents endpoint

--- server/src/routes/documents.ts
+++ server/src/routes/documents.ts
@@ -1,12 +1,25 @@
 import { Router, Request, Response } from 'express'
-
+import { auth } from '../middleware/auth.middleware'
+import Document from '../models/Document'
 export const router : Router = Router()
 
 
-router.get('/', (req : Request, res : Response) => {
-  console.log('get doc')
+router.get('/', auth, async (req : any, res : Response) => {
+  try {
+    const documents = await Document.find({ owner: req.user.userId })
+    res.json(documents)
+  } catch (e) {
+    res.status(500).json({ message: 'something went wrong, try again' })
+  }
 })
 
-router.post('/', (req : Request, res : Response) => {
-  console.log('post doc')
+router.post('/', auth, async (req : any, res : Response) => {
+  try {
+    const { title, text } = req.body
+    const document = new Document({ title, text, owner: req.user.userId })
+    await document.save()
+    res.status(201).json({ document })
+  } catch (e) {
+    res.status(500).json({ message: 'something went wrong, try again' })
+  }
 })
